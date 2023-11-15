654e7a68aba55457f1dc0aa4833d8ff5779af541
vik-splav <138093106+vik-splav@users.noreply.github.com>
2023-11-15 18:23:19 +0200

    updated notification for license

--- src/components/license/alert.tsx
+++ src/components/license/alert.tsx
@@ -1,5 +1,5 @@
 import { AlertProps } from "@/types/license";
 
 export default function Alert(props: AlertProps) {
-  return <div className={props.warn?"bg-red-600 text-white rounded-lg w-30 h-8 text-center flex items-center py-2 px-4  mr-4":" bg-sky-600 text-white rounded-lg w-30 h-8 text-center flex items-center mr-4 py-2 px-4"}><span>{props.message}</span></div>;
+  return <div className={props.warn?"bg-red-600 text-white rounded-lg w-30 h-10 text-center flex items-center py-2 px-4  mr-4":" bg-sky-600 text-white rounded-lg w-30 h-10 text-center flex items-center mr-4 py-2 px-4"}><span>{props.message}</span></div>;
 }
--- src/components/license/status.tsx
+++ src/components/license/status.tsx
@@ -6,14 +6,14 @@ export default function Status(props: LicenseStatus) {
   return (
     <div className=" absolute top-8 right-16 flex items-center text-black">
       <Alert warn={props.alert.warn} message={props.alert.message}/>
-      <span className="pr-4 text-2xl">License</span>
+      {/* <span className="pr-4 text-2xl">License</span>
       <LightMode
         style={
           props.state
             ? { color: "rgb(30, 64, 175)" }
             : { color: "rgb(171, 177, 196)" }
         }
-      />
+      /> */}
     </div>
   );
 }
