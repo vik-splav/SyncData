c61691a920c4628fcda3ecf1c79811ac081273b5
vik-splav <138093106+vik-splav@users.noreply.github.com>
2023-11-30 20:16:06 +0200

    add download file name

--- src-tauri/src/file/google.rs
+++ src-tauri/src/file/google.rs
@@ -1,15 +1,10 @@
-use serde_json::{json, Value};
-use std::collections::HashMap;
 type Myresult<T> = std::result::Result<T, Error>;
-use reqwest::header::CONTENT_DISPOSITION;
-use reqwest::multipart::{Form, Part};
 use serde::{Deserialize, Serialize};
 // use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use tokio_util::codec::{FramedRead, BytesCodec};
 use tokio::fs::File;
-use tauri::Runtime;
 use futures_util::TryStreamExt;
 
 #[derive(Debug, thiserror::Error)]
@@ -18,8 +13,6 @@ pub enum Error {
     Io(#[from] std::io::Error),
     #[error(transparent)]
     Request(#[from] reqwest::Error),
-    #[error("{0}")]
-    Custom(String),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,6 +20,11 @@ struct FileMetadata {
     name: String,
     r#type: String,
     description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Fileinfo {
+    name: String,
 }
 
 impl serde::Serialize for Error {
@@ -41,14 +39,7 @@ impl serde::Serialize for Error {
 #[tauri::command]
 pub async fn google_drive_upload(path: &str, token: &str) -> Myresult<serde_json::Value> {
     let file = File::open(path).await?;
-
-    // Create the request and attach the file to the body
     let client = reqwest::Client::new();
-    // let file_bytes = tokio::fs::read(path).await?;
-    // let file_name = Path::new(path).file_name().unwrap().to_str().unwrap();
-    // let part = Part::bytes(file_bytes);
-    // let form: Form = Form::new().part("file", part);
-
     let filetobody = filetobody(file);
     let request = client
         .post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart")
@@ -101,7 +92,7 @@ pub async fn google_drive_download(
     path: &str,
     token: &str,
     fileid: &str,
-) -> Myresult<serde_json::Value> {
+) -> Myresult<()> {
     // Create the request and attach the file to the body
     let client = reqwest::Client::new();
 
@@ -114,24 +105,22 @@ pub async fn google_drive_download(
     let requ1 = client
         .clone()
         .get(format!(
-            "https://drive.google.com/uc?export=download&id={}",
+            "https://www.googleapis.com/drive/v3/files/{}",
             fileid
         ))
         .bearer_auth(token)
         .header("Content-Type", "application/json");
     
     let response = request.send().await?;
-    println!("1");
     let bytes = response.bytes().await?;
-    println!("2");
+    let response1 = requ1.send().await?;
+    let fileinfo = response1.json::<Fileinfo>().await?;
     let mut file = std::fs::File::create(format!(
-        "{}/sd.db",
-        path
+        "{}/{}",
+        path, fileinfo.name
     ))?;
-    println!("3");
-    let writeresult = file.write_all(&bytes);
-    println!("write result {:?}", writeresult);
-    requ1.send().await?.json().await.map_err(Into::into)
+    file.write_all(&bytes).map_err(Into::into)
+    
 }
 
 #[tauri::command]
